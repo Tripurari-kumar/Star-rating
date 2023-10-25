4d0bd2e8ca50e99566978f141d7416a5b7380900
tripurari kumar <36576242+Tripurari-kumar@users.noreply.github.com>
2023-10-25 23:16:23 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+let container= document.getElementById('star-container');
+let stars= container.querySelectorAll('.star');
+
+let filled=0;
+
+function onStarClick(currentClicked, eventType){
+  if(eventType==='clicked'){
+    filled=currentClicked;
+    let ratingCount= document.querySelector('.rating-count');
+    ratingCount.innerText =` RatingCount: ${filled}`;
+  }
+  for(let i = 0 ; i < currentClicked; i++){
+    stars[i].classList.add('filled');
+  }
+  for(let i=currentClicked ;i<stars.length;i++){
+    stars[i].classList.remove('filled');
+  }
+}
+
+container.addEventListener('click',(event)=>{
+   let currentClicked= event.target.dataset.index;
+   onStarClick(currentClicked, 'clicked');
+})
+
+container.addEventListener('mouseover',(event)=>{
+  let currentHover= event.target.dataset.index;
+  onStarClick(currentHover, 'hover');
+})
+
+container.addEventListener('mouseout',()=>{
+  onStarClick(filled, 'hover');
+})
+
+
+
